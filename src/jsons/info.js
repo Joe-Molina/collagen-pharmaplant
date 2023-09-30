53dddf243808fab237a3c7c29e92aedf4ebce933
Joe Molina <joedodaniljr123@gmail.com>
2023-09-29 21:23:13 -0400

    first commit

---
+++ src/jsons/info.js
@@ -0,0 +1,13 @@
+export const Ficha = {
+  introduction:
+    "In the constant search for quality products, PHARMAPLANT offers you COLLAGEN HYDROLYZED MULTI WITH HYALURONIC ACID, BIOTIN AND VITAMIN C. A product that has been formulated with a mixture of active ingredients that improve generalhealth, each of them fulfilling specific bodily functions.",
+
+  collagen:
+    "Collagen is the most abundant protein in the human body, also known as a structural protein that helps maintain the union in the body,which is why it is known as a 'natural glue'. Over time and with increasingly deficient diets, the presence of collagen in the body starts to decrease, and we can notice this by having less shiny skin.less healthy hair, sagging, and joint stiffness. All of these are signs of aging in the human body,and although it is an irreversible process, we can slow it down a little by consuming supplements richin collagen and hyaluronic acid.",
+
+  natural:
+    "100% natural supplement, created under the strictest health standards.",
+
+  benefits:
+    "Our product is created using collagen peptides. Collagen peptides provide multiple health benefits. Being a smaller molecule that is quickly absorbed, they provide benefits to the musculoskeletal system,cardiovascular system, and immune system and improve the health of the skin, nails, and hair.",
+};
